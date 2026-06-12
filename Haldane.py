fb4bb5b9a4010ade6590b544fca79659815033e4
Ben Burgess <ben.burgess@hotmail.co.uk>
2026-06-12 10:40:06 +0100

    Added phases to separable potential

--- Haldane.py
+++ Haldane.py
@@ -168,8 +168,8 @@ def V_wpl(x, y, beta=1, V=1, a=1, phi_1=0, phi_2=0):
     G2 = G * np.array([0, 1])
     return V * (np.cos((G1[0]+G2[0])*x + (G1[1]+G2[1])*y + phi_1) + np.cos((G1[0]-G2[0])*x + (G1[1]-G2[1])*y + phi_2))
 
-def V_sep(x, y, beta=1, V=1, a=1):
+def V_sep(x, y, beta=1, V=1, a=1, phi_1=0, phi_2=0):
     G = beta * 4 * np.pi / (np.sqrt(3) * a)
     G1 = G * np.array([np.sqrt(3)/2, -1/2])
     G2 = G * np.array([0, 1])
-    return V * (np.cos(G1[0]*x + G1[1]*y) + np.cos(G2[0]*x + G2[1]*y))
+    return V * (np.cos(G1[0]*x + G1[1]*y + phi_1) + np.cos(G2[0]*x + G2[1]*y + phi_2))
