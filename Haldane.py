08eefc4e660da1034bc3cf52b28807036dd2924f
Ben Burgess <ben.burgess@hotmail.co.uk>
2026-06-12 11:54:29 +0100

    Added rotated potential

--- Haldane.py
+++ Haldane.py
@@ -173,3 +173,13 @@ def V_sep(x, y, beta=1, V=1, a=1, phi_1=0, phi_2=0):
     G1 = G * np.array([np.sqrt(3)/2, -1/2])
     G2 = G * np.array([0, 1])
     return V * (np.cos(G1[0]*x + G1[1]*y + phi_1) + np.cos(G2[0]*x + G2[1]*y + phi_2))
+
+def V_rot(x, y, beta=1, V=1, a=1, phi_1=0, phi_2=0, theta=0.):
+    G = beta * 4 * np.pi / (np.sqrt(3) * a)
+    G1 = G * np.array([np.sqrt(3)/2, -1/2])
+    G2 = G * np.array([0, 1])
+    R = np.array([[np.cos(theta), -np.sin(theta)],
+                  [np.sin(theta), np.cos(theta)]])
+    G1 = R @ G1
+    G2 = R @ G2
+    return V * (np.cos((G1[0]+G2[0])*x + (G1[1]+G2[1])*y + phi_1) + np.cos((G1[0]-G2[0])*x + (G1[1]-G2[1])*y + phi_2))
